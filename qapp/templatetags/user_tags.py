8a1ac4d5f57cb3155bca4e952e65d0ba5322b2ff
Mateusz Rachuta <mrachuta@thinkbox.pl>
2019-01-21 20:03:13 +0100

    Updated forms (multiple-add available!), hidded canvas object during conversion

--- qapp/templatetags/user_tags.py
+++ qapp/templatetags/user_tags.py
@@ -1,5 +1,6 @@
 from django import template
 from django.contrib.auth.models import Group
+from django.conf import settings
 
 register = template.Library()
 
@@ -14,3 +15,24 @@ def has_group(user):
 def has_group(user):
     group = Group.objects.get(name='prod')
     return group in user.groups.all()
+
+
+@register.filter(name='data_verbose')
+def data_verbose(boundField):
+    """
+    Returns field's data or it's verbose version
+    for a field with choices defined.
+
+    Usage::
+
+        {% load data_verbose %}
+        {{form.some_field|data_verbose}}
+    """
+    data = boundField.data
+    field = boundField.field
+    return hasattr(field, 'choices') and dict(field.choices).get(data,'') or data
+
+@register.filter
+def get_obj_attr(obj, attr):
+    return getattr(obj, attr)
+
